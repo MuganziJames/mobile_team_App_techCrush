0f8e755037e285632941edee70407a9b9c20ff1a
MuganziJames <jamesmuganzi35@gmail.com>
2025-06-18 12:49:52 +0300

    expo fix

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Add resolver configuration to handle React Native Firebase ES modules issue
+config.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+
 module.exports = config; 
